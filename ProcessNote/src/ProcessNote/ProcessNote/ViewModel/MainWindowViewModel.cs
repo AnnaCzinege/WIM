94ea919c5efcc121e681ce829f4a10866a338f68
esztermazi <49866977+esztermazi@users.noreply.github.com>
2020-02-19 13:38:32 +0100

    Add MainWindowViewModel class

--- ProcessNote/src/ProcessNote/ProcessNote/ViewModel/MainWindowViewModel.cs
+++ ProcessNote/src/ProcessNote/ProcessNote/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics;
+using ProcessNote.Model;
 
 namespace ProcessNote.ViewModel
 {
-    class MainWindowViewModel
+    public class MainWindowViewModel
     {
+        public Processes Processes { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Processes = new Processes();
+        }
+
+        public void GetAllProcesses()
+        {
+
+            foreach (var process in Process.GetProcesses())
+            {
+                try
+                {
+                    Processes.ProcessCollection.Add(new MyProcess()
+                    {
+
+                        Id = process.Id,
+                        Name = process.ProcessName,
+                        MemoryUsage = process.PrivateMemorySize64,
+                        StartTime = process.StartTime,
+                        RunTime = DateTime.Now - process.StartTime,
+                        Threads = process.Threads,
+
+                    });
+                }
+                catch (Win32Exception)
+                {
+
+                }
+                catch (InvalidOperationException)
+                {
+
+                }
+
+            }
+        }
     }
 }
+
